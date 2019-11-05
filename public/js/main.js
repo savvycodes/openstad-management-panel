c5eb1898438c551c761f01999cce7f68bd715c7a
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-11-05 11:03:31 +0100

    jQuery validate for idea form

--- public/js/main.js
+++ public/js/main.js
@@ -96,64 +96,12 @@ $(function () {
 
   $('.floated-chat-w .chat-messages').perfectScrollbar();
 
-  // #2. CALENDAR INIT
-
-  if ($("#fullCalendar").length) {
-    var calendar, d, date, m, y;
-
-    date = new Date();
-
-    d = date.getDate();
-
-    m = date.getMonth();
-
-    y = date.getFullYear();
-
-    calendar = $("#fullCalendar").fullCalendar({
-      header: {
-        left: "prev,next today",
-        center: "title",
-        right: "month,agendaWeek,agendaDay"
-      },
-      selectable: true,
-      selectHelper: true,
-      select: function select(start, end, allDay) {
-        var title;
-        title = prompt("Event Title:");
-        if (title) {
-          calendar.fullCalendar("renderEvent", {
-            title: title,
-            start: start,
-            end: end,
-            allDay: allDay
-          }, true);
-        }
-        return calendar.fullCalendar("unselect");
-      },
-      editable: true,
-      events: [{
-        title: "Long Event",
-        start: new Date(y, m, 3, 12, 0),
-        end: new Date(y, m, 7, 14, 0)
-      }, {
-        title: "Lunch",
-        start: new Date(y, m, d, 12, 0),
-        end: new Date(y, m, d + 2, 14, 0),
-        allDay: false
-      }, {
-        title: "Click for Google",
-        start: new Date(y, m, 28),
-        end: new Date(y, m, 29),
-        url: "http://google.com/"
-      }]
-    });
-  }
 
   // #3. FORM VALIDATION
 
-  if ($('#formValidate').length) {
-    $('#formValidate').validator();
-  }
+  $('.validate-form').each(function() {
+    $(this).validate();
+  })
 
   // #4. DATE RANGE PICKER
 
