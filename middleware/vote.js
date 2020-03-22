d00a7f2d062bbf8c922efcbfbe6ee7efa4ec2639
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-03-22 20:59:01 +0100

    hide votes

--- middleware/vote.js
+++ middleware/vote.js
@@ -2,25 +2,30 @@ const rp = require('request-promise');
 const apiUrl = process.env.API_URL;
 
 exports.allForSite = (req, res, next) => {
-  var options = {
-      uri: `${apiUrl}/api/site/${req.params.siteId}/vote`,
-      headers: {
-          'Accept': 'application/json',
-          "X-Authorization": process.env.SITE_API_KEY
-      },
-      json: true // Automatically parses the JSON string in the response
-  };
+  if (req.site.config.admin && req.site.config.admin.displayStats) {
+    var options = {
+        uri: `${apiUrl}/api/site/${req.params.siteId}/vote`,
+        headers: {
+            'Accept': 'application/json',
+            "X-Authorization": process.env.SITE_API_KEY
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
 
-  rp(options)
-    .then(function (votes) {
-       const allVotes = votes;
-       const userVotedCount = votes;
-       req.votes = allVotes;
-       res.locals.votes = allVotes;
-       next();
-    })
-    .catch(function (err) {
-      console.log('-->>>> err', err);
-      next();
-    });
+    rp(options)
+      .then(function (votes) {
+         const allVotes = votes;
+         const userVotedCount = votes;
+         req.votes = allVotes;
+         res.locals.votes = allVotes;
+         next();
+      })
+      .catch(function (err) {
+        console.log('-->>>> err', err);
+        next();
+      });
+  } else {
+    req.votes = [];
+    next();
+  }
 }
