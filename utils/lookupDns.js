4063d4b3f426cf4440c00ea00c2792ae0bd17553
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-08-31 15:31:22 +0200

    add logging

--- utils/lookupDns.js
+++ utils/lookupDns.js
@@ -3,10 +3,17 @@ const cleanUrl = require('./cleanUrl');
 
 module.exports = async (domain, timeout) => {
   return new Promise((resolve, reject) => {
-    setTimeout(() => resolve(false), timeout);
+    console.log('domain 1', domain)
 
-    dns.lookup(cleanUrl(domain), (err, address, family) => {
-      console.log('err')
+    domain = cleanUrl(domain);
+    console.log('domain 2', domain)
+    setTimeout(() => {
+      console.log('custom DNS timeout')
+      resolve(false);
+    }, timeout);
+
+    dns.lookup(, (err, address, family) => {
+      console.log('err', err)
       if(err) resolve(false);
       resolve(address);
     });
