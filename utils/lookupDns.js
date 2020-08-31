e3fee8c8dc6f71282dd665a3abc3baddcf45012d
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-08-31 15:28:30 +0200

    make sure domain is clean

--- utils/lookupDns.js
+++ utils/lookupDns.js
@@ -1,9 +1,11 @@
 const dns = require('dns');
+const cleanUrl = require('./cleanUrl');
+
 module.exports = async (domain, timeout) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => resolve(false), timeout);
 
-    dns.lookup(domain, (err, address, family) => {
+    dns.lookup(cleanUrl(domain), (err, address, family) => {
       console.log('err')
       if(err) resolve(false);
       resolve(address);
