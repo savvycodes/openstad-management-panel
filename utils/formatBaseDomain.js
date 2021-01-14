ec324d2f55ed5250547ec0aa59f07132fe78ccea
Tosh Koevoets <tosh.koevoets@gmail.com>
2021-01-14 19:17:31 +0100

    Prevent formatting database

--- utils/formatBaseDomain.js
+++ utils/formatBaseDomain.js
@@ -1,6 +1,7 @@
 const { URL } = require('url');
 
 module.exports = (domain) => {
-  const hostname = domain.startsWith('localhost') ? 'localhost' : new URL(domain).hostname;
+//  const hostname = domain.startsWith('localhost') ? 'localhost' : new URL(domain).hostname;
+  const hostname = domain.startsWith('localhost') ? 'localhost' : domain;
   return hostname;
 }
