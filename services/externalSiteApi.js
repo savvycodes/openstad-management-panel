e6fc9332d4d8b2ee0a65cd5723531857788572d8
Niels Vegter <niels@denes.nl>
2021-12-22 11:46:00 +0000

    Update backend request for templates and update new site form

--- services/externalSiteApi.js
+++ services/externalSiteApi.js
@@ -1,5 +1,5 @@
 const rp = require('request-promise');
-const apiUrl = process.env.EXTERNAL_SITE_REPO || 'https://cdn.openstad.org/template/site';
+const apiUrl = process.env.EXTERNAL_SITE_REPO || 'https://api.openstad.amsterdam.nl/api/repo';
 const siteApiKey =  process.env.SITE_API_KEY;
 
 exports.fetchAll = () => {
@@ -8,7 +8,7 @@ exports.fetchAll = () => {
     uri: `${apiUrl}`,
     headers: {
         'Accept': 'application/json',
-        "X-Authorization": siteApiKey
+//        "X-Authorization": siteApiKey
     },
     json: true // Automatically parses the JSON string in the response
   });
