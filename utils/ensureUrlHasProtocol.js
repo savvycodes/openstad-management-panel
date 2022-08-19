990bff2fa5d145f5fed0321c7531619bb94c26c2
Niels Vegter <niels@denes.nl>
2022-08-19 15:44:16 +0200

    Fix missing use of env.FORCE_HTTP

--- utils/ensureUrlHasProtocol.js
+++ utils/ensureUrlHasProtocol.js
@@ -1,5 +1,5 @@
 module.exports = (url) => {
-  const protocol = 'https';
+  const protocol = process.env.FORCE_HTTP ? 'http' : 'https';
 
   if (!url.startsWith('http://') && !url.startsWith('https://')) {
     // if no protocol, assume http
